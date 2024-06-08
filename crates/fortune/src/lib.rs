a8285da566c647370ee7c7e025eee9f68bdc88cf
Josh Nichols <josh.nichols@gusto.com>
2024-06-08 12:46:05 -0400

    extract fortune to its own crate

---
+++ crates/fortune/src/lib.rs
@@ -0,0 +1,69 @@
+use rand::Rng;
+
+use std::{
+    error::Error,
+    fmt,
+    fs::File,
+    io::{self, Read},
+    path::Path,
+};
+
+// Define our error types. These may be customized for our error handling cases.
+// Now we will be able to write our own errors, defer to an underlying error
+// implementation, or do something in between.
+#[derive(Debug, Clone)]
+pub struct NoFortunesError;
+
+// Generation of an error is completely separate from how it is displayed.
+// There's no need to be concerned about cluttering complex logic with the display style.
+//
+// Note that we don't store any extra info about the errors. This means we can't state
+// which string failed to parse without modifying our types to carry that information.
+impl fmt::Display for NoFortunesError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "no fortunes found in file")
+    }
+}
+
+// from https://github.com/zuisong/rs-fortune
+pub struct Fortunes(Vec<String>);
+impl Fortunes {
+    pub fn new(content: String) -> Result<Fortunes, Box<dyn Error>> {
+        let fortunes = content.split("\n%\n").map(|it| it.to_string()).collect();
+        Ok(Self(fortunes))
+    }
+
+    pub fn from_file(path: &String) -> Result<Fortunes, Box<dyn Error>> {
+        let file_path = Path::new(&path);
+        if !file_path.exists() {
+            return Err(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    format!("The fortune file '{path}' does not exist"),
+            )
+                .into());
+        }
+        if file_path.is_dir() {
+            return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    format!("'{path}' is a directory, not a file"),
+            )
+                .into());
+        }
+        let mut file = File::open(file_path)?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+
+        Self::new(content)
+    }
+
+    pub fn choose_one(&self) -> Result<&String, NoFortunesError> {
+        let fortunes = &self.0;
+        if fortunes.is_empty() {
+            return Err(NoFortunesError);
+        }
+        let mut rng = rand::thread_rng();
+        let index = rng.gen_range(0..fortunes.len());
+
+        Ok(&fortunes[index])
+    }
+}
--- crates/intro/src/main.rs
+++ crates/intro/src/main.rs
@@ -2,77 +2,10 @@ use figlet_rs::FIGfont;
 use rand::{
     thread_rng,
     seq::SliceRandom,
-    Rng,
-};
-use std::{
-    fmt,
-    error::Error,
-    fs::File,
-    io::{self, Read},
-    path::Path,
 };
 
-// Define our error types. These may be customized for our error handling cases.
-// Now we will be able to write our own errors, defer to an underlying error
-// implementation, or do something in between.
-#[derive(Debug, Clone)]
-struct NoFortunesError;
-// Generation of an error is completely separate from how it is displayed.
-// There's no need to be concerned about cluttering complex logic with the display style.
-//
-// Note that we don't store any extra info about the errors. This means we can't state
-// which string failed to parse without modifying our types to carry that information.
-impl fmt::Display for NoFortunesError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "no fortunes found in file")
-    }
-}
+use fortune::{NoFortunesError, Fortunes};
 
-// from https://github.com/zuisong/rs-fortune
-struct Fortunes(Vec<String>);
-impl Fortunes {
-    pub fn new(content: String) -> Result<Fortunes, Box<dyn Error>> {
-        let fortunes = content
-            .split("\n%\n")
-            .map(|it| it.to_string())
-            .collect();
-        Ok(Self(fortunes))
-    }
-
-    pub fn from_file(path: &String) -> Result<Fortunes, Box<dyn Error>> {
-        let file_path = Path::new(&path);
-        if !file_path.exists() {
-            return Err(io::Error::new(
-                io::ErrorKind::NotFound,
-                format!("The fortune file '{path}' does not exist"),
-            )
-            .into());
-        }
-        if file_path.is_dir() {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                format!("'{path}' is a directory, not a file"),
-            )
-            .into());
-        }
-        let mut file = File::open(file_path)?;
-        let mut content = String::new();
-        file.read_to_string(&mut content)?;
-
-        Self::new(content)
-    }
-
-    pub fn choose_one(&self) -> Result<&String, NoFortunesError> {
-        let fortunes = &self.0;
-        if fortunes.is_empty() {
-            return Err(NoFortunesError);
-        }
-        let mut rng = rand::thread_rng();
-        let index = rng.gen_range(0..fortunes.len());
-
-        Ok(&fortunes[index])
-    }
-}
 
 fn choose_fortune() -> Result<String, NoFortunesError> {
     let fortune_path = String::from("/opt/homebrew/opt/fortune/share/games/fortunes/intro");
@@ -116,8 +49,8 @@ fn random_font() -> String {
 }
 
 fn main() {
-    let fortune = match choose_fortune() {
-        Ok(fortune) => fortune,
+    let message = match choose_fortune() {
+        Ok(message) => message,
         Err(_) => {
             println!("No fortunes found");
             return;
@@ -125,9 +58,7 @@ fn main() {
     };
 
     let font_choice = random_font();
-    let figure_result = figlet(font_choice, fortune);
-
-    let figure = match figure_result {
+    let figure = match figlet(font_choice, message) {
         Ok(figure) => figure,
         Err(_) => {
             println!("Could not generate figure");
