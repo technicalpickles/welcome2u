a43f0b3cc115b3c585d70aa8d58ec99491780eb8
Josh Nichols <josh.nichols@gusto.com>
2024-06-07 20:37:53 -0400

    refactor user info to a class

--- crates/user/src/main.rs
+++ crates/user/src/main.rs
@@ -1,10 +1,38 @@
 use users::{get_user_by_uid, get_current_uid};
 
+struct UserInfo {
+    username: String,
+    hostname: String,
+}
+
+impl UserInfo {
+    fn new(username: String, hostname: String) -> Self {
+        Self {
+            username,
+            hostname,
+        }
+    }
+
+    fn username(&self) -> &str {
+        &self.username
+    }
+    fn hostname(&self) -> &str {
+        &self.hostname
+    }
+
+    fn collect() -> Self {
+        let user = get_user_by_uid(get_current_uid()).unwrap();
+        let username = user.name().to_str().unwrap();
+
+        let hostname = hostname::get().unwrap();
+        let hostname_str = hostname.to_str().unwrap();
+
+        Self::new(username.to_string(), hostname_str.to_string())
+    }
+}
+
 fn main() {
-    let user = get_user_by_uid(get_current_uid()).unwrap();
-    let username = user.name().to_str().unwrap();
-    let hostname = hostname::get().unwrap();
-    let hostname_str = hostname.to_str().unwrap();
-    
-    println!("{}@{}", username, hostname_str);
+    let info = UserInfo::collect();
+
+    println!("{}@{}", info.username(), info.hostname());
 }
