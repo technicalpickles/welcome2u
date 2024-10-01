45727eaf6581cf1b5cc2d714145d93b6cbb306d6
Josh Nichols <josh.nichols@gusto.com>
2024-10-01 08:52:15 -0400

    refactor command segment into its own crate

---
+++ segments/command/src/lib.rs
@@ -0,0 +1,67 @@
+use std::process::{Command, Stdio, ExitStatus};
+use anyhow::Result;
+use thiserror::Error;
+use display::MotdSegment;
+
+#[derive(Debug)]
+pub struct CommandSegment {
+    command : String,
+    output: String,
+}
+
+impl CommandSegment {
+    pub fn new(command: &str) -> Self{
+        Self {
+            command: command.to_string(),
+            output: "".to_string()
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum CommandError {
+    #[error("command `{command_ran:?}` failed with status `{status:?}`")]
+    CommandFailed {
+        command_ran: String,
+        status: ExitStatus,
+    },
+    #[error("failed to spawn command")]
+    SpawnFailed(#[from] std::io::Error),
+    #[error("couldn't parse output as utf8")]
+    OutputParseError(#[from] std::string::FromUtf8Error),
+}
+
+impl MotdSegment for CommandSegment {
+    fn prepare(&mut self) -> Result<()> {
+        // TODO can we avoid cloning this?
+        let mut command = Command::new(self.command.clone());
+
+        let child = match command.stdout(Stdio::piped()).spawn() {
+            Ok(child) => child,
+            Err(error) => return Err(CommandError::SpawnFailed(error).into())
+        };
+
+        let output = child.wait_with_output()?;
+
+        if !output.status.success() {
+            let error = CommandError::CommandFailed {
+                // TODO can we avoid a clone?
+                command_ran: self.command.clone(),
+                status: output.status,
+
+            };
+            return Err(error.into())
+        }
+        match String::from_utf8(output.stdout) {
+            Ok(string) => self.output = string,
+            Err(error) => return Err(CommandError::OutputParseError(error).into())
+        }
+
+        Ok(())
+    }
+
+    fn render(&self) -> Result<()> {
+        print!("{}", self.output);
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,8 @@
-use std::process::{Command, Stdio, ExitStatus};
-
 use std::env;
 use anyhow::Result;
-use thiserror::Error;
 use anyhow::Context;
 
 use display::MotdSegment;
-
-#[derive(Debug)]
-struct CommandSegment {
-    command : String,
-    output: String,
-}
-
-impl CommandSegment {
-    fn new(command: &str) -> Self{
-        Self {
-            command: command.to_string(),
-            output: "".to_string()
-        }
-    }
-}
-
-#[derive(Error, Debug)]
-enum CommandError {
-    #[error("command `{command_ran:?}` failed with status `{status:?}`")]
-    Failed {
-        command_ran: String,
-        status: ExitStatus,
-    },
-    #[error("failed to spawn command")]
-    SpawnFailed(#[from] std::io::Error),
-
-    #[error("couldn't parse output as utf8")]
-    OutputParseError(#[from] std::string::FromUtf8Error),
-}
-
-impl MotdSegment for CommandSegment {
-    fn prepare(&mut self) -> Result<()> {
-        // TODO can we avoid cloning this?
-        let mut command = Command::new(self.command.clone());
-
-        let child = match command.stdout(Stdio::piped()).spawn() {
-            Ok(child) => child,
-            Err(error) => return Err(CommandError::SpawnFailed(error).into())
-        };
-
-        let output = child.wait_with_output()?;
-
-        if !output.status.success() {
-            let error = CommandError::Failed {
-                // TODO can we avoid a clone?
-                command_ran: self.command.clone(),
-                status: output.status,
-
-            };
-            return Err(error.into())
-        }
-        match String::from_utf8(output.stdout) {
-            Ok(string) => self.output = string,
-            Err(error) => return Err(CommandError::OutputParseError(error).into())
-        }
-
-        Ok(())
-    }
-
-    fn render(&self) -> Result<()> {
-        print!("{}", self.output);
-        Ok(())
-    }
-}
 
 fn main() -> Result<()> {
     env::set_var("BASE_DIR", ".");
@@ -79,7 +12,7 @@ fn main() -> Result<()> {
         Box::<heading::HeadingSegment>::default(),
         Box::<quote::FortuneHeaderSegment>::default(),
         Box::new(<user::UserSegment>::default()),
-        Box::new(CommandSegment::new("target/debug/os")),
+        Box::new(<command::CommandSegment>::new("target/debug/os")),
         // Box::new(CommandSegment::new("modules/20-uptime")),
         // Box::new(CommandSegment::new("modules/30-load")),
         Box::new(<memory::MemorySegment>::default()),
